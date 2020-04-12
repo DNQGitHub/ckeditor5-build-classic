f2a91b61940e351dec72c0dbe3e5ea9f9a920025
DNQ <dnq.httt@gmail.com>
2020-04-12 18:10:11 +0700

    Update utils.js

--- src/mediaembed/utils.js
+++ src/mediaembed/utils.js
@@ -1,13 +1,16 @@
+import { isWidget } from '@ckeditor/ckeditor5-widget/src/utils';
+
 export function isMediaEmbed( modelElement ) {
 	return !!modelElement && modelElement.is( 'media' );
 }
 
 export function isMediaEmbedWidget( viewElement ) {
-	return !!viewElement.getCustomProperty( 'media' ) && isMediaEmbed( viewElement );
+	return !!viewElement.getCustomProperty( 'media' ) && isWidget( viewElement );
 }
 
 export function getSelectedMediaEmbedWidget( selection ) {
 	const viewElement = selection.getSelectedElement();
+
 	if ( viewElement && isMediaEmbedWidget( viewElement ) ) {
 		return viewElement;
 	}
